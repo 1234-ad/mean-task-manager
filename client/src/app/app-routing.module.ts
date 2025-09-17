86701287a9f01aabc010d886a813b811f455de3d
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-17 14:03:51 +0530

    Add Angular routing configuration

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TaskListComponent } from './components/tasks/task-list/task-list.component';
+import { TaskDetailComponent } from './components/tasks/task-detail/task-detail.component';
+import { ProjectListComponent } from './components/projects/project-list/project-list.component';
+import { ProjectDetailComponent } from './components/projects/project-detail/project-detail.component';
+import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
+
+import { AuthGuard } from './guards/auth.guard';
+import { GuestGuard } from './guards/guest.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [GuestGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'tasks', component: TaskListComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/:id', component: TaskDetailComponent, canActivate: [AuthGuard] },
+  { path: 'projects', component: ProjectListComponent, canActivate: [AuthGuard] },
+  { path: 'projects/:id', component: ProjectDetailComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: UserProfileComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/dashboard' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
